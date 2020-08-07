2c604b42a2c2435027f63a65cf3309739caede7d
juliomchado <juliocarlos00@hotmail.com>
2020-08-07 17:36:28 +0100

    Challenge completed

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,24 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const verifyEmailExists = await this.customersRepository.findByEmail(email);
+
+    if (verifyEmailExists) {
+      throw new AppError('Email already exists');
+    }
+
+    const createCustomer = await this.customersRepository.create({
+      name,
+      email,
+    });
+
+    return createCustomer;
   }
 }
 
--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -13,10 +13,25 @@ interface IRequest {
 
 @injectable()
 class CreateProductService {
-  constructor(private productsRepository: IProductsRepository) {}
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
 
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    // TODO
+    const checkNameProduct = await this.productsRepository.findByName(name);
+
+    if (checkNameProduct) {
+      throw new AppError('Procuct already registered');
+    }
+
+    const createProduct = await this.productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    return createProduct;
   }
 }
 
